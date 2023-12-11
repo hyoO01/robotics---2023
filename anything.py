ae8f629cb6fa96cf4a7ca3ebfc982abd89ce1f40
hyoO01 <jhe0316@kakao.com>
2023-12-12 01:39:28 +0900

    merge conflict

--- anything.py
+++ anything.py
@@ -1,3 +1,12 @@
+if __name__ == "__main__":
+    print("hello python, hi ros.")
+    print("Python is the best language for ros")
+
+~/workspace/robotics-schl$ git status
+~/workspace/robotics-schl$ git diff
+~/workspace/robotics-schl$ git add .
+~/workspace/robotics-schl$ git commit -m 'edit anything.py'
+
 from add_lists import add_lists
 
 if __name__ == "__main__":
